362b50a6216e9814ab2e6bd84d0e919518f75582
FrancescoVargas <francesco.marin@mail.polimi.it>
2022-05-23 11:00:20 +0200

    messages and aboutyourcar

--- lib/cars_user.dart
+++ lib/cars_user.dart
@@ -29,58 +29,91 @@ class _Cars_userState extends State<Cars_user> {
                 onPressed: () {
                   Navigator.pop(context);
                 })),
-        body: ListView.builder(
-            itemCount: cars == [] ? 0 : cars.length,
-            itemBuilder: (context, index) {
-              return Container(
-                  padding: const EdgeInsets.all(8),
-                  child: Expanded(
-                      child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                        const SizedBox(height: 20),
-                        Container(
-                            height: 50,
-                            width: 350,
-                            child: MaterialButton(
+        body: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+              const SizedBox(height: 40),
+              (cars.isEmpty)
+                  ? Container(
+                      height: 70,
+                      width: 350,
+                      child: const Text('No cars yet :(',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              color: Colors.redAccent,
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold)),
+                      padding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          border:
+                              Border.all(width: 5.0, color: Colors.redAccent),
+                          boxShadow: [
+                            BoxShadow(
                                 color: Colors.grey.shade200,
-                                onPressed: () async {
-                                  String suspOrAct = '';
-                                  if (cars[index].activeOrNot == 't') {
-                                    suspOrAct = 'Suspend';
-                                  } else {
-                                    suspOrAct = 'Active';
-                                  }
-                                  final newCars = await Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) =>
-                                              InfoCar(cars[index], suspOrAct)));
-                                  if (newCars != []) {
-                                    setState(() {
-                                      cars = newCars;
-                                    });
-                                  }
-                                },
-                                child: Text(
-                                    cars[index].vehicle.toString() +
-                                        '-' +
-                                        cars[index].model.toString(),
-                                    style: const TextStyle(
-                                        color: Colors.redAccent,
-                                        fontSize: 35,
-                                        fontWeight: FontWeight.w500))),
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(12),
-                                boxShadow: const [
-                                  BoxShadow(
-                                      color: Colors.redAccent,
-                                      spreadRadius: 6,
-                                      blurRadius: 2)
-                                ]))
-                      ])));
-            }),
+                                spreadRadius: 6,
+                                blurRadius: 2)
+                          ]))
+                  : ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: cars == [] ? 0 : cars.length,
+                      itemBuilder: (context, index) {
+                        return Container(
+                            padding: const EdgeInsets.all(8),
+                            child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  const SizedBox(height: 20),
+                                  Container(
+                                      height: 50,
+                                      width: 350,
+                                      child: MaterialButton(
+                                          color: Colors.grey.shade200,
+                                          onPressed: () async {
+                                            String suspOrAct = '';
+                                            if (cars[index].activeOrNot ==
+                                                't') {
+                                              suspOrAct = 'Suspend';
+                                            } else {
+                                              suspOrAct = 'Active';
+                                            }
+                                            final newCars =
+                                                await Navigator.push(
+                                                    context,
+                                                    MaterialPageRoute(
+                                                        builder: (context) =>
+                                                            InfoCar(cars[index],
+                                                                suspOrAct)));
+                                            if (newCars != []) {
+                                              setState(() {
+                                                cars = newCars;
+                                              });
+                                            }
+                                          },
+                                          child: Text(
+                                              cars[index].vehicle.toString() +
+                                                  '-' +
+                                                  cars[index].model.toString(),
+                                              style: const TextStyle(
+                                                  color: Colors.redAccent,
+                                                  fontSize: 35,
+                                                  fontWeight:
+                                                      FontWeight.w500))),
+                                      decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(12),
+                                          boxShadow: const [
+                                            BoxShadow(
+                                                color: Colors.redAccent,
+                                                spreadRadius: 6,
+                                                blurRadius: 2)
+                                          ]))
+                                ]));
+                      })
+            ])),
         floatingActionButton: FloatingActionButton(
             onPressed: () async {
               final newCars = await Navigator.push(context,
--- lib/message_page.dart
+++ lib/message_page.dart
@@ -5,7 +5,7 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 class MessagePage extends StatefulWidget {
-  List<String> messages = [];
+  List<String> messages;
   MessagePage(this.messages, {Key? key}) : super(key: key);
 
   @override
@@ -13,7 +13,7 @@ class MessagePage extends StatefulWidget {
 }
 
 class MessagePageState extends State<MessagePage> {
-  List<String> messages = [];
+  List<String> messages;
   MessagePageState(this.messages, {Key? key});
 
   late FirebaseMessaging messaging;
@@ -37,17 +37,73 @@ class MessagePageState extends State<MessagePage> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(title: const Text("My first app")),
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(messages.last),
-        ],
-      )),
-    ));
+    return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+            backgroundColor: Colors.redAccent,
+            title: const Text('PrCar'),
+            automaticallyImplyLeading: false,
+            leading: IconButton(
+                icon: const Icon(Icons.arrow_back),
+                onPressed: () {
+                  Navigator.pop(context);
+                })),
+        body: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+              const SizedBox(height: 40),
+              (messages.isEmpty)
+                  ? Container(
+                      height: 70,
+                      width: 350,
+                      child: const Text('No messages yet :(',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              color: Colors.redAccent,
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold)),
+                      padding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          border:
+                              Border.all(width: 5.0, color: Colors.redAccent),
+                          boxShadow: [
+                            BoxShadow(
+                                color: Colors.grey.shade200,
+                                spreadRadius: 6,
+                                blurRadius: 2)
+                          ]))
+                  : ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: messages == [] ? 0 : messages.length,
+                      itemBuilder: (context, index) {
+                        return Container(
+                            padding: const EdgeInsets.all(8),
+                            child: Expanded(
+                                child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: [
+                                  const SizedBox(height: 20),
+                                  Container(
+                                      height: 50,
+                                      width: 350,
+                                      child: Text(messages[index]),
+                                      decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(12),
+                                          boxShadow: const [
+                                            BoxShadow(
+                                                color: Colors.redAccent,
+                                                spreadRadius: 6,
+                                                blurRadius: 2)
+                                          ]))
+                                ])));
+                      }),
+            ])));
   }
 
   /*ciao() async {
