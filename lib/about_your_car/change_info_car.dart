a3fae7b61d3529b3fe36962e6efe485a86ea3b59
beppe22 <giuseppe.italia@mail.polimi.it>
2022-06-07 23:28:05 +0100

    On line

--- lib/about_your_car/change_info_car.dart
+++ lib/about_your_car/change_info_car.dart
@@ -202,10 +202,6 @@ class _ChangeInfoCarState extends State<ChangeInfoCar> {
             leading: IconButton(
                 icon: const Icon(Icons.arrow_back),
                 onPressed: () {
-<<<<<<< HEAD
-                  setState(() {});
-=======
->>>>>>> 6d464c9dd6644e55cbc43517470f849e787d3945
                   Navigator.pop(context, carModel);
                 }),
             actions: [
