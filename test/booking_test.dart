30981c7df7a0ec3071fb659af53fe8085ec3cd4b
beppe22 <giuseppe.italia@mail.polimi.it>
2022-06-11 14:20:14 +0100

    On line

--- test/booking_test.dart
+++ test/booking_test.dart
@@ -1,17 +1,23 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:prcarpolimi/booking.dart';
 import 'package:prcarpolimi/main.dart';
 
 void main() async {
-  //WidgetsFlutterBinding.ensureInitialized();
-  //await Firebase.initializeApp();
-
   test('Counter value should be incremented', () async {
-    var myapp = const MyApp();
-    runApp(myapp);
+    //WidgetsFlutterBinding.ensureInitialized();
+    //TestWidgetsFlutterBinding.ensureInitialized();
+    //await Firebase.initializeApp();
+    //var myapp = const MyApp();
+    //runApp(myapp);
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp();
+    FirebaseFirestore.instance.settings = const Settings(
+        host: 'localhost:8080', sslEnabled: false, persistenceEnabled: false);
+    runApp(const MyApp());
     final booking = BookingOut('ElLKXX1ddNgfwtMThtRmq7pw8l42Audi449605',
         'ElLKXX1ddNgfwtMThtRmq7pw8l42', '15/06/2023-16/06/2023');
 
